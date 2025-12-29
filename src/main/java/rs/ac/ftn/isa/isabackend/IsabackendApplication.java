2fd381eba91cfda0f30c4420932b410ae6b6254c
c4ps63 <pavlekapuran1@gmail.com>
2025-12-29 15:23:54 +0100

    Initial commit - Spring Boot backend

---
+++ src/main/java/rs/ac/ftn/isa/isabackend/IsabackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.isa.isabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/ftn/isa/isabackend/IsabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.isa.isabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
